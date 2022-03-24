201ac5ff435cb51ec66745f909275f9395c1c133
abelardovacca <abelardovacca@gmail.com>
2022-03-23 23:49:12 -0400

    Mejorando el modelo de clases

--- Almacen/src/comercio/main/Main.java
+++ Almacen/src/comercio/main/Main.java
@@ -1,7 +1,8 @@
 package comercio.main;
 
 import comercio.productos.Producto;
-import comercio.productos.Verdura;
+import comercio.productos.ProductoEnvasado;
+import comercio.productos.ProductoPorUnidad;
 
 import java.util.*;
 
@@ -10,18 +11,18 @@ public class Main {
 
         List<Producto> productos = new ArrayList<>();
 
-        Producto CocaColaZero = new Producto("Coca-Cola Zero", "Litros: 1.5", "Precio: ", 20);
-        Producto CocaCola = new Producto("Coca-Cola", "Litros: 1.5", "Precio: " , 18);
-        Producto ShampooSedal = new Producto("Shampoo Sedal", "Contenido: 500ml", "Precio: ", 19);
-        Verdura Frutillas = new Verdura("Frutillas", "Unidad de venta: kilo", "Precio: ", 64);
+        ProductoEnvasado cocaColaZero = new ProductoEnvasado("Coca-Cola Zero", "Litros: 1.5", 20);
+        ProductoEnvasado cocaCola = new ProductoEnvasado("Coca-Cola", "Litros: 1.5", 18);
+        ProductoEnvasado shampooSedal = new ProductoEnvasado("Shampoo Sedal", "Contenido: 500ml", 19);
+        ProductoPorUnidad frutillas = new ProductoPorUnidad("Frutillas", "Unidad de venta: kilo", 64);
 
-        productos.add(CocaColaZero);
-        productos.add(CocaCola);
-        productos.add(ShampooSedal);
-        productos.add(Frutillas);
+        productos.add(cocaColaZero);
+        productos.add(cocaCola);
+        productos.add(shampooSedal);
+        productos.add(frutillas);
 
         for (Producto productos1 : productos) {
-            System.out.println(productos1.obtenerDetalles());
+            System.out.println(productos1.toString());
         }
         System.out.println("=============================");
         System.out.println("Producto más caro: " + Collections.max(productos).getNombre());
--- Almacen/src/comercio/productos/Producto.java
+++ Almacen/src/comercio/productos/Producto.java
@@ -2,14 +2,10 @@ package comercio.productos;
 
 public class Producto implements Comparable<Producto> {
     private String nombre;
-    private String cantidad;
-    private String precio;
     private int valor;
 
-    public Producto(String nombre, String cantidad, String precio, int valor) {
+    public Producto(String nombre, int valor) {
         this.nombre = nombre;
-        this.cantidad = cantidad;
-        this.precio = precio;
         this.valor = valor;
     }
 
@@ -21,22 +17,6 @@ public class Producto implements Comparable<Producto> {
         this.nombre = nombre;
     }
 
-    public String getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(String cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public String getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(String precio) {
-        this.precio = precio;
-    }
-
     public int getValor() {
         return valor;
     }
@@ -45,12 +25,9 @@ public class Producto implements Comparable<Producto> {
         this.valor = valor;
     }
 
-    public String obtenerDetalles() {
-        return "Nombre: " + this.nombre + " /// " + this.cantidad + " /// " + this.precio + this.valor;
-    }
-
     @Override
     public int compareTo(Producto o) {
         return this.getValor() - o.getValor();
     }
+    
 }
